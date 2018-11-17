ce3519dd3142dc7112469c180b93787a1fff7006
Luís Freitas <luisfreitas@MacBook-Pro-4.local>
2018-11-17 22:11:18 +0000

    first commit

---
+++ examples/sensor-temperature.js
@@ -0,0 +1,45 @@
+const MAM_public = require('../lib/attachDataPublic.js')
+const MAM_private = require('../lib/attachDataPrivate.js')
+const MAM_restricted = require('../lib/attachDataRestricted.js')
+
+const IOTA = require('iota.lib.js')
+const iota = new IOTA()
+
+let timeLoop,date, 
+	i=1
+
+if( process.argv[2] == undefined){          //Getting the time in seconds for the loop
+  timeLoop = 60000                       //default 1 minute
+} else {
+  timeLoop = process.argv[2]*1000 
+}
+
+console.log('\n\nSENDING DATA!!\n\n')
+
+//Create a JSON as message
+
+function start(){
+	date = new Date(Date.now()).toLocaleString()
+    let message = { 
+        'Temperature': (Math.random() * (30.0 - 20.0) + 20.0).toFixed(2),
+        'Unit': 'ºC',
+        'Message' : i, 
+        'Date' : date          
+    }
+	switch(process.argv[3]){								//Getting the mode of the stream (Public:1, Private:2, Restricted: 3)
+		case '1': MAM_public.attach(message);break; 
+		case '2': MAM_private.attach(message);break;
+		case '3': MAM_restricted.attach(message);break;
+		default: MAM_public.attach(message)
+	}
+	console.log('Start sending data to Tangle...')
+	let messageS = JSON.stringify(message)
+	console.log('Message: %s',messageS)
+	console.log('Message in trytes: ' + iota.utils.toTrytes(messageS))
+	console.log('--------------------------------------------------------------------------------------------------------------------')
+	i++
+}
+
+setInterval(function(){
+	start()
+},timeLoop)
